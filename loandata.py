e46a379237c7d01bad0504fdfb0b17508e71a3b4
Rupasree05 <rupasreesaladi@gmail.com>
2025-03-28 21:30:08 +0530

    Update loandata.py

--- loandata.py
+++ loandata.py
@@ -5,6 +5,7 @@ model=joblib.load('loan_data1.joblib')
 Gender=st.number_input('enter the gender Male:0 Female:1')
 Married=st.number_input('enter  martial status unmarried:0 married:1')
 Income=st.number_input('Loan ammount in thousands')
+la=st.number_input('Loan Amount in thousands')
 if st.button('predict Approval'):
     predictions=model.predict([[Gender,Married,Income,la]])
     if predictions=='y':
